7629045830b5546a605c70e32a65f182fbf6e793
Aubin Joanes Igiraneza <aubinigiraneza1@gmail.com>
2025-12-03 08:30:22 +0200

    aaaa

---
+++ django_blog/blog/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Post, Comment, Tag
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'content')
+    list_filter = ('author', 'published_date')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    search_fields = ('content',)
+    list_filter = ('created_at', 'author')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
