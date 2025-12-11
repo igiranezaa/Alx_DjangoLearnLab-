9c691a60939f16017a91ee91d1022d2a6101c0f1
Aubin Joanes Igiraneza <163933994+igiranezaa@users.noreply.github.com>
2025-12-11 10:55:01 +0200

    Refactor user serializers and add extra fields
    Refactor user serializers to use get_user_model and include bio and profile_picture fields in registration.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,42 +1,50 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
-from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
-
-
-class RegisterSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        # ALX requires EXACTLY this:
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email', ''),
+            email=validated_data.get('email'),
             password=validated_data['password']
         )
-        Token.objects.create(user=user)
+
+        # Optional extra fields
+        user.bio = validated_data.get('bio', '')
+        user.profile_picture = validated_data.get('profile_picture', None)
+
+        user.save()
         return user
 
 
-class LoginSerializer(serializers.Serializer):
+class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
-    def validate(self, data):
-        user = authenticate(
-            username=data['username'],
-            password=data['password']
-        )
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password")
+
+        user = authenticate(username=username, password=password)
+
         if not user:
             raise serializers.ValidationError("Invalid username or password")
-        data['user'] = user
-        return data
+        attrs["user"] = user
+        return attrs
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
